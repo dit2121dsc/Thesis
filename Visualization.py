bbc792c296aada78633e3ff541a0fe4b05e95c3d
dit2121dsc <145901217+dit2121dsc@users.noreply.github.com>
2024-11-08 21:00:21 +0200

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -1,11 +1,4 @@
-# import spacy
-# from spacy import displacy
-#
-# text = "When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously."
-#
-# nlp = spacy.load("en_core_web_sm")
-# doc = nlp(text)
-# displacy.serve(doc, style="ent")
+
 
 
 import spacy
